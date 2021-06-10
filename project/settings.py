69bc033e107480f93aa62135ccec861512043b03
ShancoVils <81937926+ShancoVils@users.noreply.github.com>
2021-06-10 04:31:19 +0300

    Update settings.py

--- project/settings.py
+++ project/settings.py
@@ -132,7 +132,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_REDIRECT_URL = 'main'
 LOGOUT_REDIRECT_URL = 'main'
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'artgallery/media')
