b83a7846a684acd2af1e77dd1ccb357fed447e0b
ShancoVils <81937926+ShancoVils@users.noreply.github.com>
2021-06-10 03:50:02 +0300

    Update settings.py

--- project/settings.py
+++ project/settings.py
@@ -125,7 +125,7 @@ STATIC_URL = '/static/'
 #STATIC_DIR = os.path.join(BASE_DIR, 'static')
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 PROJECT_ROOT = os.path.join(os.path.abspath(__file__))
-
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
